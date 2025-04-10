4881d0d03cf4cd405104b2102936fe524dada695
Bao <notname2210@gmail.com>
2025-04-10 22:42:11 +0700

    change_url_api_backend

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
 
         const[cartItems,setCartItems] = useState({});
-        const url = "http://localhost:5000";
+        const url = "https://food-order-backend-5afp.onrender.com";
         const [token,setToken] = useState("");
         const[food_list,setFoodList] = useState([]);
         
